2503632868544e21dad73afc42616d2bd199fee0
Patrick Steranka <Patrick@Steranka.com>
2022-07-08 16:03:21 -0400

    End 3.28

--- renderer.js
+++ renderer.js
@@ -1,3 +1,21 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+const { desktopCapturer } = require('electron')
+
+
+document.getElementById('screenshot-button').addEventListener( 'click', () => {
+
+  desktopCapturer.getSources({ types:['window'], thumbnailSize:{width:1920, height:1080} })
+  .then( sources => {
+
+    console.log(sources)
+
+    document.getElementById('screenshot').src = sources[0].thumbnail.toDataURL()
+  })
+
+})
