365cef6c7b5c0bdb2dbee93fbda609049c8691cc
Patrick Steranka <Patrick@Steranka.com>
2022-07-09 13:15:29 -0400

    Starting an Electron app to find duplicate files based on hashs. This app just prints hashes for all files in current directory.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // Modules
 const {app, BrowserWindow, globalShortcut} = require('electron')
 const windowStateKeeper = require('electron-window-state');
+const crypto = require('crypto');
+const fs = require('fs');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -54,9 +56,42 @@ function createWindow () {
   })
 }
 
+function getHashFor(path){
+  const sha1 = new Promise((resolve, reject) => {
+    const hash = crypto.createHash('sha1')
+    const rs = fs.createReadStream(path)
+    rs.on('error', reject)
+    rs.on('data', chunk => hash.update(chunk))
+    rs.on('end', () => resolve(hash.digest('hex')))
+  });
+  return sha1;
+
+}
+
 // Add the functionality of the lesson in this function
 function runLesson(){
+  let waitingFor = new Array();
+  // Get a list of files (max 22)
+  const directoryPath = __dirname; /* path.join(__dirname, 'Documents'); */
 
+//passsing directoryPath and callback function
+  fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+      return console.log('Unable to scan directory: ' + err);
+    }
+    //listing all files using forEach
+    files.forEach(function (file) {
+      // Do whatever you want to do with the file
+      let isFile = fs.lstatSync(file).isFile();
+      if (isFile){
+        getHashFor(file).then((hash) => {
+          console.log('File: ' + file + '\t\tHash: ' + hash);
+        })
+      }
+    });
+  });
+  console.log('EXIT runLesson');
 }
 
 // Electron `app` is ready
